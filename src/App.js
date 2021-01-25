fc086ba578bce7399db290844c78e133bfa96770
zpb <p.b.zakharov@gmail.com>
2021-01-25 12:07:47 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const val = 100
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Hello world
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
